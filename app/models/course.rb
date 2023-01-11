5635f03d720a863e6c6b98fea0972c619fece73c
Ezra Njeru <njerukithinji872@gmail.com>
2023-01-11 22:11:07 +0300

    added controllers

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+    belongs_to :school
+    belongs_to :university
 end
+ 
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,2 +1,4 @@
 class University < ApplicationRecord
+    has_many :schools
+    has_many :courses, through: :schools
 end
